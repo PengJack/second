d9d5859c4f39370839c46dec59f123b0072e8226
PengJack <497768342@qq.com>
2019-03-07 20:04:22 +0800

    添加 js面试算法题.js 文件
    添加 js面试算法题.js 文件

---
+++ js/js面试算法题.js
@@ -0,0 +1,11 @@
+// 数组去重，假设没有复杂数据类型
+// 方法一： 通过对象中转
+var arr = [1,2,'a', 'b', 'a', 1, 2, 'd'];
+var newArr = [];
+var obj = {};
+arr.forEach(function(item, index) {
+  if(!obj[item]) {
+    obj[item] = item;
+    newArr.push(item);
+  }
+})
